2ee60505d7c0ce8e38860c9716c5ac58623b78db
oriaca <oriaca372m@outlook.jp>
2020-11-01 15:41:36 +0900

    [git-now] 11/01(日) 15:41

--- src/ast/nodes/compile-time-value.ts
+++ src/ast/nodes/compile-time-value.ts
@@ -1,8 +1,18 @@
 import * as prim from 'Src/ast/nodes/primitive'
 
 import { ValueType } from 'Src/ast/langtype'
+import { LangFunction } from 'Src/ast/langfunction'
 
+export type Overload = LangFunction[]
+
+type CompileTimeValueType = Overload
 export class CompileTimeValue implements prim.TypedNode {
+	constructor(private readonly _value: CompileTimeValueType) {}
+
+	get value(): CompileTimeValueType {
+		return this._value
+	}
+
 	get type(): ValueType {
 		throw 'CompileTimeValueのtypeを取得することは出来ない'
 	}
