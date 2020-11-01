cfe54bd545805e0b23b30490f6cb1afd8c4cb950
oriaca <oriaca372m@outlook.jp>
2020-11-01 16:53:02 +0900

    [git-now] 11/01(日) 16:53

--- src/ast/overload-resolver/index.ts
+++ src/ast/overload-resolver/index.ts
@@ -2,10 +2,24 @@ import * as u from 'Src/utils'
 import { Expr } from 'Src/ast/nodes/expr'
 import { LangFunction } from 'Src/ast/langfunction'
 
-export function resolveOverload(candidate: LangFunction[], _args: Expr[]): LangFunction {
-	if (candidate.length !== 1) {
-		u.notImplemented()
+function calcScore(candidate: LangFunction, args: Expr[]): number {
+	const ok = u.l.zip(candidate.argTypes, args).every(([t, e]) => {
+		if (t === undefined || e === undefined) {
+			return false
+		}
+
+		return t.equals(e.type.core)
+	})
+
+	return ok ? 1 : 0
+}
+
+export function resolveOverload(candidates: LangFunction[], args: Expr[]): LangFunction {
+	for (const candidate of candidates) {
+		if (calcScore(candidate, args) === 1) {
+			return candidate
+		}
 	}
 
-	return candidate[0]
+	throw 'マッチするオーバーロード候補がない'
 }
