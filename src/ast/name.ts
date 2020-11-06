c9fcc681bbb5569d8eac6d2fd81b6eb1c73f1f5d
oriaca <oriaca372m@outlook.jp>
2020-11-06 22:04:22 +0900

    [git-now] 11/06(金) 22:04

--- src/ast/name.ts
+++ src/ast/name.ts
@@ -1,9 +1,7 @@
-import { TypeCore } from 'Src/ast/langtype'
 import { Variable } from 'Src/ast/variable'
 import { CtVariable } from 'Src/ast/compile-time'
 
 export type NameValueType =
-	| { kind: 'type'; value: TypeCore }
 	| { kind: 'variable'; value: Variable }
 	| { kind: 'ct-variable'; value: CtVariable }
 
